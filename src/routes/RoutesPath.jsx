431d5b6ba9faa66be02a91050db924b447da7bf4
pritam sarkar <pritam7796419792@gmail.com>
2024-07-31 03:06:53 +0530

    login page is done

--- src/routes/RoutesPath.jsx
+++ src/routes/RoutesPath.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import MainPage from "../components/mainPage/MainPage";
 import Admin from "../components/MainComponents/adminPage/Admin";
-
+import LoginPage from "../components/LoginPage/LoginPage";
 function RoutesPath() {
   return (
     <Routes>
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/" element={<MainPage />} />
       <Route path="/admin" element={<Admin />} />
     </Routes>
